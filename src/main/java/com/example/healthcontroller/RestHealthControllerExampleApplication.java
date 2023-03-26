223da2db8e9250ed3a6390a113b7f57dae158644
lbanko24 <leon.banko@fer.hr>
2023-03-27 01:16:56 +0200

    first commit

---
+++ src/main/java/com/example/healthcontroller/RestHealthControllerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestHealthControllerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestHealthControllerExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/healthcontroller/RestHealthControllerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.healthcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestHealthControllerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
